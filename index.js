01e023f6899b29f36d97bb93aa3967e61520e463
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-11-10 02:41:08 -0300

    Fix cors

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const graphqlHttp = require("express-graphql");
 const graphQlSchema = require("./schema/index");
 const graphQlResolvers = require("./resolvers/index");
 const isAuth = require("./middleware/is-auth");
+
+const cors = require('cors');
 import models from "./models";
 
 const app = express();
@@ -20,7 +22,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-
+app.use(cors());
 app.use(isAuth);
 
 app.use(
