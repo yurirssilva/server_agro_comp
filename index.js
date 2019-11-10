73af7c90cd0acd48792982b61f79701277097b63
Yuri Rodrigues Santos Silva <yurirssilva@gmail.com>
2019-11-10 02:54:05 -0300

    Cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+app.all((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
