59321539a48a9688c194c80a1291f25fc1720b76
Dalton <dalton_jss@hotmail.com>
2019-11-10 00:25:33 -0300

    conserto do conn sequelize

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 import Sequelize from "sequelize";
 require('dotenv/config');
 
-const sequelize = new Sequelize(process.env.DB_HOST, process.env.DB_NAME, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql"
 });
 
